1ca5988d3b1859481a9e69d2c4eafbefdc988c77
Devyn Giles <gilesdevyn6@gmail.com>
2024-02-28 16:52:04 -0700

    Added some things and updated the main page

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 const myUsername = document.getElementById("Username");
 const username = localStorage.getItem("userName");
 myUsername.textContent = `Username: ${username}`;
+
+let ratingList1 = {};
+let ratingList2 = {};
+let ratingList3 = {};
