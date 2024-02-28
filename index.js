2fa9b621594a720ba14dffa1ec74ad77dcd906b3
Devyn Giles <gilesdevyn6@gmail.com>
2024-02-28 14:05:33 -0700

    Added some more stuff

---
+++ index.js
@@ -0,0 +1,7 @@
+function login() {
+  const nameEl = document.querySelector("#name");
+  localStorage.setItem("userName", nameEl.value);
+  const passwordEl = document.querySelector("#password");
+  localStorage.setItem("password", passwordEl.value);
+  window.location.href = "main.html";
+}
