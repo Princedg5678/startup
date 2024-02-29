3526c2672d37e0109a3437f53442fbe7ca4aa3ec
Devyn Giles <gilesdevyn6@gmail.com>
2024-02-28 18:01:32 -0700

    Added database

--- main.js
+++ main.js
@@ -2,12 +2,15 @@ const myUsername = document.getElementById("Username");
 const username = localStorage.getItem("userName");
 myUsername.textContent = `Username: ${username}`;
 
-let ratingList1 = [];
-let ratingList2 = [];
-let ratingList3 = [];
+let ratingLists = { ratingList1: [], ratingList2: [], ratingList3: [] };
 
-const rating1 = document.getElementById("Username");
-const rating2 = document.getElementById("Username");
-const rating3 = document.getElementById("Username");
+// const rating1 = document.getElementById("Username");
+// const rating2 = document.getElementById("Username");
+// const rating3 = document.getElementById("Username");
 
-function rateItem(item) {}
+function rateItem(item) {
+  const ID = "rating" + item;
+  const rating = document.getElementById(ID).value;
+  const key = "ratingList" + item;
+  ratingLists[key].add(rating);
+}
