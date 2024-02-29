60c95412d03811eafe6059a15be6d55311a8c8c3
Devyn Giles <gilesdevyn6@gmail.com>
2024-02-29 12:24:25 -0700

    Added some database stuff

--- main.js
+++ main.js
@@ -6,7 +6,23 @@ let ratingLists = { ratingList1: [], ratingList2: [], ratingList3: [] };
 
 function rateItem(item) {
   const ID = "rating" + item;
-  const rating = document.getElementById(ID).value;
+  const rating = Number(document.getElementById(ID).value);
   const key = "ratingList" + item;
-  ratingLists[key].add(rating);
+  ratingLists[key].push(rating);
+
+  const aID = "average" + item;
+  const avg = document.getElementById(aID);
+  const newAverage = findAverageRating(item);
+  avg.textContent = "Average Rating: " + newAverage;
+}
+
+function findAverageRating(item) {
+  const key = "ratingList" + item;
+  const ratings = ratingLists[key];
+  let sum = 0;
+  for (rating of ratings) {
+    sum = sum + rating;
+  }
+  let average = sum / ratings.length;
+  return average;
 }
