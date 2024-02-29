3c9f1c1c3dd0f6ed2e0a1f8bf859bea5773f0c35
Devyn Giles <gilesdevyn6@gmail.com>
2024-02-29 12:01:52 -0700

    Added some stuff

--- main.js
+++ main.js
@@ -4,10 +4,6 @@ myUsername.textContent = `Username: ${username}`;
 
 let ratingLists = { ratingList1: [], ratingList2: [], ratingList3: [] };
 
-// const rating1 = document.getElementById("Username");
-// const rating2 = document.getElementById("Username");
-// const rating3 = document.getElementById("Username");
-
 function rateItem(item) {
   const ID = "rating" + item;
   const rating = document.getElementById(ID).value;
