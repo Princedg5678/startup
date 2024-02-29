9d3da5ad76c36fdd87db34e2703f6ebcd6855ceb
Devyn Giles <gilesdevyn6@gmail.com>
2024-02-28 17:54:44 -0700

    added some stuff

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ const myUsername = document.getElementById("Username");
 const username = localStorage.getItem("userName");
 myUsername.textContent = `Username: ${username}`;
 
-let ratingList1 = {};
-let ratingList2 = {};
-let ratingList3 = {};
+let ratingList1 = [];
+let ratingList2 = [];
+let ratingList3 = [];
+
+const rating1 = document.getElementById("Username");
+const rating2 = document.getElementById("Username");
+const rating3 = document.getElementById("Username");
+
+function rateItem(item) {}
