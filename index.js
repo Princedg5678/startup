50f1d441f0e048f955e75a50f12c1dab2472659b
Devyn Giles <gilesdevyn6@gmail.com>
2024-03-22 16:27:32 -0600

    Added some stuff

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const app = express();
-//const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
 const bcrypt = require("bcrypt");
-const DB = require("database.js");
+const DB = require("./database.js");
 
 // The service port. In production the front-end code is statically hosted by the service on the same port.
-const port = process.argv.length > 2 ? process.argv[2] : 3000;
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
 // JSON body parsing using built-in middleware
 app.use(express.json());
